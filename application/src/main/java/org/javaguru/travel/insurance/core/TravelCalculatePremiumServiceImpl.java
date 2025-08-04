30fbc336782f2937254c38d1ce8f816fada7a84d
Dinara Andrianova <ish.dinara@gmail.com>
2025-08-04 17:48:31 +0300

    STEP 035 - DONE

--- application/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ application/src/main/java/org/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -13,16 +13,22 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor(access = AccessLevel.PACKAGE)
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+
     private final TravelCalculatePremiumRequestValidator requestValidator;
     private final DateTimeService dateTimeService;
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
         List<ValidationError> errors = requestValidator.validate(request);
-        if (!errors.isEmpty()) {
-            return new TravelCalculatePremiumResponse(errors);
-        }
+        return errors.isEmpty() ? buildResponse(request) : buildResponse(errors);
 
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(List<ValidationError> errors) {
+        return new TravelCalculatePremiumResponse(errors);
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(TravelCalculatePremiumRequest request) {
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
         response.setPersonFirstName(request.getPersonFirstName());
         response.setPersonLastName(request.getPersonLastName());
